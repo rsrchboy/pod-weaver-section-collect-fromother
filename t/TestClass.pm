1446a94ddaff8433a31c692b83bcb5a2824f1ee5
Chris Weyl <cweyl@alumni.drew.edu>
2012-02-28 12:53:12 -0800

    add tests

---
+++ t/TestClass.pm
@@ -0,0 +1,35 @@
+package TestClass;
+
+1;
+
+__END__
+
+=head1 SIMPLE SECTION
+
+Something, something
+
+=head1 SECTION AND COMMAND
+
+Something, something
+
+=head2 woo!
+
+yada, yada
+
+=head1 LIST
+
+=over 4
+
+=item Bla
+
+Whee
+
+=item BlaBla
+
+=back
+
+=head1 LAST SECTION
+
+Yay!
+
+=cut
